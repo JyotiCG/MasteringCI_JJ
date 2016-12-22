e0c98f6e380d3c35a3ca4503ca931c5c13e1d2b0
Jha <jyoti.jha@capgemini.com>
2016-12-22 09:52:15 +0530

    bankingApp

---
+++ BankingApplication/src/ICICIBank.java
@@ -0,0 +1,49 @@
+import java.util.LinkedList;
+
+import com.capgemini.exception.InsufficientBalanceException;
+import com.capgemini.exception.InvalidAccountNumberException;
+import com.capgemini.model.Account;
+
+public class ICICIBank {
+	
+	
+	LinkedList<Account> accounts=new LinkedList<>();
+	
+	
+	public String createAccount(int accountNumber,int amount)
+	{
+		Account account = new Account();
+		
+		accounts.add(account);
+		
+		return "account created successfully";
+	}
+	
+	private Account searchAccount(int accountNumber)throws InvalidAccountNumberException
+	{
+		
+		for(Account account : accounts)
+		{
+			if(account.getAccountNumber()==accountNumber)
+			{
+				return account;
+			}
+		}
+		throw new InvalidAccountNumberException();
+		
+	}
+	
+	public int withdrawAmount(int accountNumber,int amount) throws InvalidAccountNumberException, InsufficientBalanceException
+	{
+		Account account=searchAccount(accountNumber);
+		
+		if((account.getAmount()-amount)>=0)
+		{
+			account.setAmount(account.getAmount()-amount);
+			return account.getAmount();
+		}
+		
+		throw new InsufficientBalanceException();
+	}
+
+}
